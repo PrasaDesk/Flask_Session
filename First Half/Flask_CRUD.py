88f732b1423fcee6cd616f6a60c0e95de8317260
Prasad Yeole <prasad.yeole@mindbowser.com>
2019-09-15 14:22:31 +0530

    Id added in Flask_Demo.py

--- First Half/Flask_CRUD.py
+++ First Half/Flask_CRUD.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request #import objects from the Flask model
 app = Flask(__name__) #define app using Flask
 
-languages = [{'name' : 'JavaScript'}, {'name' : 'Python'}, {'name' : 'Ruby'}]
+languages = [{'id': 1,'name' : 'JavaScript'}, {'id': 2,'name' : 'Python'}, {'id': 1,'name' : 'Ruby'}]
 
 
 @app.route('/', methods=['GET'])
@@ -14,31 +14,31 @@ def returnAll():
 	return jsonify({'languages' : languages})
 
 
-@app.route('/lang/<string:name>', methods=['GET'])
-def returnOne(name):
-	langs = [language for language in languages if language['name'] == name]
+@app.route('/lang/<string:id>', methods=['GET'])
+def returnOne(id):
+	langs = [language for language in languages if language['id'] == int(id)]
 	return jsonify({'language' : langs[0]})
 
 
 @app.route('/lang', methods=['POST'])
 def addOne():
-	print(request.json['name'])
-	language = {'name' : request.json['name']}
+	print(request.json)
+	language = {'id': request.json['id'], 'name' : request.json['name']}
 
 	languages.append(language)
-	return jsonify({'languages' : languages})
+	return jsonify({'msg': 'Language has been added.!'})
 
 
-@app.route('/lang/<string:name>', methods=['PUT'])
-def editOne(name):
-	langs = [language for language in languages if language['name'] == name]
+@app.route('/lang/<string:id>', methods=['PUT'])
+def editOne(id):
+	langs = [language for language in languages if language['id'] == int(id)]
 	langs[0]['name'] = request.json['name']
 	return jsonify({'language' : langs[0]})
 
 
-@app.route('/lang/<string:name>', methods=['DELETE'])
-def removeOne(name):
-	lang = [language for language in languages if language['name'] == name]
+@app.route('/lang/<string:id>', methods=['DELETE'])
+def removeOne(id):
+	lang = [language for language in languages if language['id'] == int(id)]
 	languages.remove(lang[0])
 	return jsonify({'languages' : languages})
 
